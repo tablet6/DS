3079abe09026ef97e67132a2eed811414c08e7fc
tablet6 <tablet6@gmail.com>
2016-05-07 07:21:56 -0700

    no message

---
+++ Array/array1/array1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  array1
+//
+//  Created by sagark17 on 4/24/16.
+//  Copyright © 2016 SaSa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Stack/StackAsLL/StackAsLL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StackAsLL
+//
+//  Created by sagardev on 4/25/16.
+//  Copyright © 2016 s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
